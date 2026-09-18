19478a2aca7fae1ef1b1835e2a882b31b8f442bc
Krishnaswamy Subramanian <jskswamy@gmail.com>
2026-09-18 06:08:46 +0530

    Upgrade ccstatusline to v2.2.30
    Updates the npm tarball hash for the new release. Verified the
    built binary reports 2.2.30.

--- pkgs/ccstatusline.nix
+++ pkgs/ccstatusline.nix
@@ -4,11 +4,11 @@
 _: prev: {
   ccstatusline = prev.stdenv.mkDerivation rec {
     pname = "ccstatusline";
-    version = "2.2.27";
+    version = "2.2.30";
 
     src = prev.fetchurl {
       url = "https://registry.npmjs.org/ccstatusline/-/ccstatusline-${version}.tgz";
-      hash = "sha256-T2Cb3tENjBBkUWzvuQLtWTkasru6l9WT6KEtB+LaWMI=";
+      hash = "sha256-NWR5zB/3Nbdmvrom7ploLN7xQ8YDsPTx/X5qVugn12k=";
     };
 
     nativeBuildInputs = [prev.makeWrapper];
